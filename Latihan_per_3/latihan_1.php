5b7e84eec3284f95c86274aab4a19bc0ac3f79db
Wans20 <92008232+praks27@users.noreply.github.com>
2022-03-23 11:46:46 +0700

    update

---
+++ Latihan_per_3/latihan_1.php
@@ -0,0 +1,96 @@
+<?php
+//COBA 1
+$nilai=array(60,70,80,75,90);
+//fungsi_rsort
+rsort($nilai);
+//fungsi_sort
+sort($nilai);
+var_dump($nilai);
+//echo $nilai[2];
+echo "<br>";
+
+
+//COBA 2
+$dtnilai=array("dina"=>80,"sinta"=>90,"nia"=>85);
+
+var_dump($dtnilai);
+//echo $dtnilai["sinta"];
+echo "<br>";
+
+
+//COBA 3(contoh perulangan for)
+for($i = 2; $i<=13; $i++){
+    echo $i.",";
+}
+echo "<br>";
+
+
+//COBA 4(contoh perulangan While)
+$a=0;
+while($a <=13){
+    if($a >=2){
+    echo $a.",";
+    }
+    $a++;
+}
+echo "<br>";
+
+//COBA 5(perulangan do while)
+$x=0;
+do{
+    if($x >=2){
+    echo $x.",";
+    }
+    $x++;
+}while ($x <=13);
+echo "<br>";
+
+//COBA 6(perulangan)
+for($c =1; $c <=30; $c++){
+    if ($c%2 == 0){
+        echo "<b>".$c.", "."</b>";
+        }else{
+            echo"<u>".$c." ,"."</u>";
+        }
+}
+echo "<br>";
+
+//COBA 7(foreach)
+//cara menghitung jumlah Array (count(x)/sezeof(x))
+$mhs = array("Adit","Agung","Ardi","Galang","Putra","Sulthan");
+foreach($mhs as $m){
+    echo $m.".";
+}
+echo $mhs[1]."<br>";
+
+
+//Latihan 
+$toko_ase=array(
+    array("kode"=>"B01","nama"=>"Buku","harga"=>"50.000"),
+    array("kode"=>"B02","nama"=>"Pulpen","harga"=>"10.000"),
+    array("kode"=>"B03","nama"=>"Penghapus","harga"=>"5.000")    
+);
+foreach($toko_ase as $tka){
+    echo $tka["kode"].",".$tka["nama"].",".$tka["harga"]."<br>";
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Judul</th>
+            <th>Konten</th>
+            <th>Action</th>
+        </tr>
+    </table>
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,9 +4,199 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Project Agung Setiawan</title>
+    <link rel="stylesheet" href="bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="Assset/project.css">
+    <style type="text/css">
+    </style>
 </head>
 <body>
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-black ">
+        <div class="container-fluid ">
+          <a class="navbar-brand text-warning" href="#">Agung's page</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="position-relative" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link active text-warning text-decoration-underline"  aria-current="page" href="#">HOME</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active text-warning text-decoration-underline" target="_blank" aria-current="page" href="UAS_AGUNG/index.html">MY STORE</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active text-warning text-decoration-underline" aria-current="page" href="#contact">CONTACT US</a>
+              </li>
+              <li class="nav-item ">
+                <a class="nav-link active text-warning text-decoration-underline" target="_blank" aria-current="page" href="My-Resume .html">RESUME </a>
+              </li>
+              <!-- DROPDOWN -->
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle text-warning text-decoration-underline" href="#home" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true"  aria-expanded="false">
+                  DOWNLOAD
+                </a>
+                <div class="dropdown-menu dropdown-menu-end bg-dark" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item text-white" href="Download/Materi HTML .rar">Materi HTML</a>
+                  <a class="dropdown-item text-white" href="Download/materi-css-e_compress.rar">Materi CSS</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item text-white" href="Download/bootstrap-5.1.3-dist.rar">Materi BOOTSTRAP</a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+    <!-- header -->
+    <header class=" p-2 text-white bg-opacity-75 bg-dark" id="home">
+        <div class="container-fluid justify-content-center d-flex flex-row align-items-center">
+          <h2 class="font-2">Hello <br><span class="pe-3">I am Web Designer</span></h2>
+          <img src="IMG/Gus Dur(pluralisme).jpg"  class="rounded-circle"  width="700px" height="350px" alt="" srcset="">
+        </div>
+    </header>
+    <!-- section about-->
     
+    <section id="about" >
+      <div class="container-fluid d-flex flex-column align-items-center bg-dark text-white">
+      <h3 class="fw-bold pb-2 pt-3 text-warning">===ABOUT ME===</h3>
+      <!-- <div class=" row">
+          <div class="col-lg-2"></div> -->
+          <div class="row-cols-lg-4 text-center ">Kita sebatas hamba tuhan yang menunjukan keadilan yang sesungguhnya di muka bumi ini,dimana para setiap petinggi dengan jabatannya mereka semena mena melakukan semuanya apa yang dia inginkan tanpa memikirkan para rakyat yang masih sangat membutuhkan.
+            Dengan tahta kau semena mena menindas kami para kaum bawah yang tak punya apa apa dan hanya sebatas bisa menjujung tinggi sila kelima yang sekarang mulai sirna di negri kita, berorasi layaknya petinggi yang menjunjung nilai nilai keadilan di negri ini dan nyatanya hanya sebatas manis di mulut yang tak akan pernah kau lakukan jika kau sudah nyaman.
+            Tertawalah lepas sebelum kau menemui ajalmu sesungguhnya di dunia dan di akhirat tempat sesungguhnya ku hidup. kemulyaanmu di tangan rakyat,jika kau terus menindas rakyat,lantas bagaimana kau bisa mendapatkan kemulyaan sesungguhnya.
+            Tak usah banyak keinginan jika rakyat saja masih kelaparan. Rakyat bukan hanya sebatas makhluk yang dengan semena mena kau suruh,kau peras,kau tindas layaknya heean yang tak ada gunanya setelah kau mendapatkan apa yang kau dambakan selama ini yaitu tahta di negara yang perlahan mulai hancur dengan petinggi mereka sendiri
+            Rakyat melawan bukan berarti menghancurkan, kota hanya minta apa yang seharusnya kami terima, kota ada karna kita membela, kita hadir karna kita peduli dengan negri. Kami diam bukan berarti bungkam, kami melawan karna kami tahu sesungguhnya keadilan, semakin kamu melonjak menindas kami para kaum bawah,semakin juga hidupmu tak akan tennag meski kau mrmpunyai beribu benteng beribu pengikut beribu muka tapi kami tidak akan mundur demi keselarasan keadilan.
+          </div>
+        </div>
+  </section>
+    <!-- section blog -->
+    <section style="background-color: rgb(87, 87, 87);">
+    <br> 
+    <h3 class="fw-bold pb-2 pt-3 text-warning text-center">===MY BLOG===</h3>
+    <br>
+    <div class="container" style="position: static;">
+
+      <div class="card mb-3 bg-dark text-light" style="max-width: 1150px;">
+        <div class="row g-lg-0">
+          <div class="col-md-4">
+            <img src="IMG/Sudjiwo Tedjoe .jpg" class="img-thumbnail" alt="img">
+          </div>
+          
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Judul Artikel-1</h5>
+              <div>
+                <p class="btn btn-primary">10/11/2011</p><p style="color: blue;">Created By Wan's</p> 
+              </div>
+              <p class="card-text">1. Untuk merealisasikan diri menjadi semakin sempurna dan utuh. Dengan kata lain kehidupan bagi manusia merupakan kesempatan baginya untuk semakin merealisasikan dirinya. 
+                Kesimpulan ini berangkat dari penyelidikan atas fakta bahwa manusia tidak pernah merasa hidupnya sudah sempurna. Sebaliknya ia mengalami dirinya selalu ditarik ke atas ke arah kesempurnaan, 
+                kebijaksanaan, keutamaan, kebaikan terus-menerus. Dengan kata lain manusia tidak pernah menerima dirinya sebagai sudah sempurna, sudah utuh sampai kehidupan itu sendiri dialaminya bertemu........
+                <a href="my_blog_1.html" target="_blank"> >>>Baca Selengkapnya>>> </a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card mb-3 bg-dark text-light" style="max-width: 1150px;">
+        <div class="row g-lg-0">
+          <div class="col-md-4">
+            <img src="IMG/Najwa Syihab .jpg" class="img-thumbnail" alt="img">
+          </div>
+
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Judul Artikel-2</h5>
+              <div>
+                <p class="btn btn-primary">10/11/2011</p><p style="color: blue;">Created By Wan's</p> 
+              </div>
+              <p class="card-text">Seorang mahasiswa adalah orang yang sedang menempuh pendidikan yang lebih tinggi dari tingkatan SMA atau sederajat.
+                Menjadi seorang mahasiswa sangatlah berbeda ketika menjadi seorang siswa. Dunia kampus sangatlah berbeda dengan dunia sekolah.
+                Ada 2 hal yang harus ditanamkan ke dalam diri mahasiswa yaitu kemandirian dan integritas. Dengan adanya kemandirian, mahasiswa dituntut 
+                untuk dapat mengatur dirinya sendiri dan kegiatan sehari-hari seperti jadwal kuliah dan......
+                <a href="my_blog_2.html"> >>>Baca Lebih lanjut>>> </a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card mb-3 bg-dark text-light" style="max-width: 1150px;">
+        <div class="row g-lg-0">
+          <div class="col-md-4">
+            <img src="IMG/teknologi/technology-4.jpg" class="img-thumbnail" alt="img">
+          </div>
+
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Link Web PHP</h5>
+              <div>
+                <p class="btn btn-primary">10/11/2011</p><p style="color: blue;">Created By Wan's</p> 
+              </div>
+              <p class="card-text">HTTP adalah singkatan dari Hypertext Transfer Protocol. Sebagai bagian dari protokol HTTP, data bergerak melalui jaringan, seperti internet. Faktanya, sebagian besar informasi 
+                yang melintasi internet dilakukan melalui protokol HTTP. Banyak situs web yang menggunakan HTTP memiliki HTTP:// di awal Unique Resource Locators atau URL mereka.
+                <a href="LinkWebPHP.html"> >>>Baca Lebih lanjut>>> </a></p>
+            </div>
+          </div> 
+        </div>
+      </div>
+      <hr>
+    <!-- section contact -->
+    <form id="contact" action="#" method="post"></form>
+    <table>
+    <tr>
+        <td><label for="email" class="form-label text-warning bg-transparent">EMAIL</label></td>
+        <td><input type="email" name="txt_email" form="control" id="txt_email" class="form-control p-2 bg-transparent" placeholder="email@example.com te"></td>
+    </tr>
+    <tr>
+      <td><label for="uname" class="form-label text-warning">NAMA LENGKAP</label></td>
+      <td><input type="text" class="form-control p-2 bg-transparent" id="nama" name="nama"></td>
+    </tr>
+    <tr>
+      <td>
+        <label for="" class=" text-warning">INFORMATION </label>
+      </td>
+      <td>
+        <input type="text" name="information" class="form-control p-2 bg-transparent" id="information" placeholder="Sharing Article" >
+      </td>
+    </tr>
+    <tr>
+      <td class=" text-warning">
+        KETERANGAN 
+      </td>
+        <td> 
+          <textarea name="keterangan" id="keterangan" cols="130" rows="15" class="form-control bg-transparent"></textarea>
+          <br>
+          <button type="submit" name="btn_submit" id="btn_submit" class="align-items-center">SIMPAN</button>
+          <button type="reset" name="btn_reset" id="btn_reset" class="justify-content-center">RESET</button> 
+        </td>
+     </tr> 
+  </table>
+</section>
+
+<!-- footer -->
+<footer class=" bg-black p-4"><h3 class="text-lg-center"></h3>
+  <div class="row">
+    <div class="col-1 col-md-3"></div>
+      <div class="col-2 col-md-5">
+        <h5 class="text-warning">HEAD OFFICE</h5><br>
+          <p class=" text-white">NAME : AGUNG SETIAWAN</p>
+          <p class="text-white">ADDRESS : Street Rejosari Gg.01 Rt.05 Rw.03 No.04</p>
+          <p class="text-white">Benowo, Pakal, Surabaya, Jawa Timur, Indonesia</p>
+          <p><a href="+6282139873061" class=" text-white" target="_blank">TELEPHONE : 082139873061</a></p>
+          <p><a href="mailto:wans200102@gmail.com" class=" text-white" target="_blank">EMAIL : wans200102@gmail.com</a>
+      </div>
+      <div class="col-3 col-md-3">
+        <h5 class="text-warning">SOCIAL MEDIA</h5><br>
+          </i>
+          <p><a href="https://www.facebook.com/profile.php?id=100010256894133" class=" text-white" target="_blank">Facebook</a></p>
+          <p><a href="https://www.instagram.com/a_wan_s/" class=" text-white" target="_blank">Instagram</a></p>
+          <p><a href="https://twitter.com/tak_banyak_kata?t=F6xtrMVsmyXjUaFZOqf6Kg&s=09" class=" text-white" target="_blank">Twitter</a></p>
+          <p><a href="http://wa.me/+6282139873061" class=" text-white" target="_blank">WhatsApp</a></p>
+      </div>
+    </div>
+  </footer>
+    <!-- link:script -->
+    <script src="Materi/bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
 </body>
-</html>
+</html>   
